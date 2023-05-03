9ed9e2a8d261278d4cb902a04416fbcf1988847e
Chriss <mathias393@hotmail.com>
2023-05-03 01:19:35 -0300

    Subi todo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+/*git remote add origin https://github.com/ChrissmaVip/Trivia.git
+git branch -M main
+git push -u origin main*/
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Boba la que lee");
+    }
+}
