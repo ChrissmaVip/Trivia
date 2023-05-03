3336c268299dbff671eead460fb084310eb8578b
Chriss <mathias393@hotmail.com>
2023-05-03 01:24:11 -0300

    segunda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Boba la que lee");
+        System.out.println("Soy pro");
     }
 }
